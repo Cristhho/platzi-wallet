3217040202f1167638c931dbb27baa40879c5dca
cristhian ochoa <cristhho@gmail.com>
2020-03-18 23:24:04 -0500

    patron proxy

---
+++ app/src/main/java/com/cristianvillamil/platziwallet/ui/transfer/TransferProxy.kt
@@ -0,0 +1,21 @@
+package com.cristianvillamil.platziwallet.ui.transfer
+
+class TransferProxy {
+    val MAX_TRANSFER_AMOUNT = 1000000
+    val MIN_TRANSFER_AMOUNT = 100
+
+    fun checkTransfer(amount: Double): String {
+        return when{
+            amount > MAX_TRANSFER_AMOUNT -> "La transaccion excede el monto máximo"
+            amount < MIN_TRANSFER_AMOUNT -> "La transaccion debe ser mayor a $MIN_TRANSFER_AMOUNT"
+            else -> {
+                doTransfer(amount)
+                "La transferencia se hizo correctamente"
+            }
+        }
+    }
+
+    private fun doTransfer(amount: Double) {
+
+    }
+}
